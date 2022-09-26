256df64fbdec973c1331c30666696d4cdb71f190
bayan2002 <bayanabdelbaryq@gmail.com>
2022-09-26 20:06:40 +0300

    resolve conflict releates #14

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 require('env2')('.env');
-const routeuserRouterr = require('./routes/userRoutes');
+const router = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT || 5000);
@@ -15,8 +15,7 @@ app.use(compression());
 app.use(cookieParser());
 app.disable('x-powered-by');
 
-
-app.use(routeuserRouterr);
+app.use(router);
 app.get('/data', (req, res) => {
   res.send({ a: 'jk' });
 });
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,6 @@
 const userRouter = require('express').Router();
 const signup = require('../controllers/user/signup');
 
-userRouter.get('/test', (req, res) => res.send('TESTED PASSED YEEEAHH!!'));
 userRouter.post('/api/v1/users/signup', signup);
 
 module.exports = userRouter;
