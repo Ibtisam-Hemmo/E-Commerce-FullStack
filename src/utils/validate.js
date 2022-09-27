e408942d617dfbfa16e508cb3abecc1b2d27b3c6
bayan2002 <bayanabdelbaryq@gmail.com>
2022-09-27 10:31:40 +0300

    Resolve Conflict and Insert fake Data

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
 const signUpSchema = Joi.object({
-  username: Joi.string().required(),
-  Email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
   img: Joi.string().required(),
   password: Joi.string().min(8).required(),
   confirmPassword: Joi.string().required().valid(Joi.ref('password')),
