5638172b8bc5c580be447a89a5d48b2bff0286e0
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-09-27 11:06:13 +0300

    Edit Sql And Insert Some Data & resolve Conlict

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 require('env2')('.env');
-// const router = require('./routes');
+const router = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT || 5000);
@@ -15,10 +15,7 @@ app.use(compression());
 app.use(cookieParser());
 app.disable('x-powered-by');
 
-// app.use(router);
-app.get('/data', (req, res)=>{
-    res.send({a: 'jk'})
-})
+app.use(router);
 
 if(process.env.NODE_ENV=='production'){
     app.use(express.static(join(__dirname, '..', 'client', 'build')));
--- src/controllers/user/signup.js
+++ src/controllers/user/signup.js
@@ -3,7 +3,34 @@ const { signUpSchema } = require('../../utils/validate');
 const CustomError = require('../../utils/CustomError')
 
 const signup = (req, res, next) => {
-    console.log('signup');
+  const {
+    username, email, img, password, confirmPassword,
+  } = req.body;
+  signUpSchema.validateAsync(req.body)
+    .then(() => userByEmail(email))
+    .then((rows) => {
+      if (rows.rowCount) {
+        throw new CustomError('Email already Exists', 409);
+      }
+      return password;
+    })
+    .then((pass) => hash(pass, 10))
+    .then((hashing) => addUser(req.body, hashing))
+    .then((data) => {
+      const { id } = data.rows[0];
+      const { user_name } = data.rows[0];
+      return generateToken({ id, username: user_name });
+    })
+    .then((token) => res.status(201)
+      .cookie('token', token, { httpOnly: true })
+      .json({ message: 'Success', status: 201 }))
+    .catch((err) => {
+      if (err.details) {
+        next(new CustomError(err.details[0].message, 400));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports = signup;
